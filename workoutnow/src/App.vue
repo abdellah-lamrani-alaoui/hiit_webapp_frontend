080da6259af2d9bdf1029858c63d4fc59b06e2d0
abdellah <abdellah@scibids.com>
2021-02-08 19:36:17 +0100

    First commit

---
+++ workoutnow/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <GenerateWorkout></GenerateWorkout>
+</template>
+
+<script>
+import GenerateWorkout from './components/GenerateWorkout.vue'
+
+export default {
+  name: 'App',
+  components: {
+    GenerateWorkout
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ workoutnow/src/components/GenerateWorkout.vue
@@ -0,0 +1,50 @@
+<template>
+    <div id="app">
+      <p v-if="duration_minutes"><b>This is a {{ duration_minutes }} minutes workout !</b> &#128170; &#128170; &#128170;</p>
+      <div>
+        <a v-if="timer_url" :href="timer_url" target="_blank"><button class="btn2">Get the Timer </button></a>
+      </div>
+      <br>
+      <img v-if="image_url" :src="image_url"><br>
+      <button @click="generate_workout">{{ button_caption }}</button>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: "GenerateWorkout",
+  data () {
+  return {image_url : "", timer_url : "", duration_minutes : "", button_caption : "Generate Workout"}
+  },
+  methods: {
+    generate_workout: function() {
+      this.button_caption = "Change Workout";
+      axios.get("http://localhost:8000/workout").then(response => (this.image_url = response.data.image_url, this.timer_url = response.data.timer_url, this.duration_minutes = response.data.duration_minutes))
+      }
+    }
+};
+</script>
+
+<style>
+button {
+  padding: 10px 20px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  font-size: 14px;
+  font-family: '微软雅黑',arail;
+  cursor: pointer;
+  background-color: #6495ED;
+  color: #fff;
+}
+.btn2 {
+  padding: 10px 20px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  font-size: 10px;
+  font-family: '微软雅黑',arail;
+  cursor: pointer;
+  background-color: #51678d;
+  color: #fff;
+}
+</style>
