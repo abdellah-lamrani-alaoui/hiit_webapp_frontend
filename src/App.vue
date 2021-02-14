b032a21a63ae676ca6bf23fa825e83fe4b930838
abdellah <abdellah@scibids.com>
2021-02-14 20:36:55 +0100

    Reduce margin top

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <img alt="Vue logo" src="./assets/smaller_logo_vectorized.png">
   <GenerateWorkout></GenerateWorkout>
   <br>
-  <br>
 </template>
 
 <script>
@@ -23,6 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
 }
 </style>
